f07fa30a29400f66d836862063014dca8b0f5473
DEN2621 <danya_dante_2001@mail.ru>
2022-11-14 23:42:25 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserContext db;
+        public HomeController(UserContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            return View(db.Users.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(User user)
+        {
+            db.Users.Update(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult UserInfo(int id)
+        {
+            return View(db.Users.Find(id));
+        }
+        [HttpPost]
+        public IActionResult UserInfo()
+        {
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Delete(int? id)
+        {
+            db.Entry(new User() { Id = id.Value }).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
